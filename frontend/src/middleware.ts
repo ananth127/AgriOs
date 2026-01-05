2c5229a48920b1d60aea8588a1be9af5d5b24adb
ananthxorstack <ananthxorstack@gmail.com>
2026-01-05 10:55:45 +0530

    V2

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,11 +1,12 @@
 import createMiddleware from 'next-intl/middleware';
+import { locales, defaultLocale } from './i18n/config';
 
 export default createMiddleware({
     // A list of all locales that are supported
-    locales: ['en', 'hi', 'kn', 'ta', 'te', 'ml', 'mr'],
+    locales,
 
     // Used when no locale matches
-    defaultLocale: 'en'
+    defaultLocale
 });
 
 export const config = {
